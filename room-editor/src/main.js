0cc5f6bb0340469dfef39dd0bd97e5aeb21f26f7
Iceteerapat <Iceteerapat.k@hotmail.com>
2025-04-08 17:57:58 +0700

    config

---
+++ room-editor/src/main.js
@@ -0,0 +1,54 @@
+import { createApp } from 'vue';
+
+import Toast from "vue-toastification";
+import PrimeVue from 'primevue/config'
+import ConfirmationService from 'primevue/confirmationservice';
+import DialogService from 'primevue/dialogservice';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(router);
+app.use(Toast);
+app.use(ConfirmationService);
+app.use(DialogService);
+
+app.config.globalProperties.$filters = {
+    formatDateTime(value) {
+      return moment(value).format('DD/MM/yyyy HH:mm:ss.SSS');
+    },
+    formatDateNormalTime(value) {
+      return moment(value).format('DD/MM/yyyy HH:mm:ss');
+    },
+    formatDateTimeNormal(value) {
+      return moment(value).format('DD/MM/yyyy HH:mm');
+    },
+    formatDateTimeOnlyDate(value) {
+      return moment(value).format('DD/MM/yyyy');
+    }, 
+    formatDateShort(value) {
+      return moment(value).format('YYMMDD');
+    },
+    formatDateTimeMonthName(value) {
+      return moment(value).format('DD MMMM YYYY');
+    },
+    formatDdMmYyyy(value) {
+      return moment(value, 'YYYYMMDD').format('DD/MM/YYYY');
+    },
+    formatDateReport(value) {
+      return moment(value,).format('DD-MMM-YYYY');
+    },
+    formatDecimal(value) {
+      return numeral(value).format('0.00');
+    },
+    formatAmount(value) {
+      return numeral(value).format('0,0.00');
+    },
+    formatAmountFiveDigit(value) {
+      return numeral(value).format('0,0.00000');
+    }
+  }
+
+app.mount('#app');
