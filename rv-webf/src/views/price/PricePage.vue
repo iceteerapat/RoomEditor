72f9e757e52a88e5e79ca5522cfc84f9cf432933
lankeiceza01 <iceteerapat.k@hotmail.com>
2025-06-03 19:28:59 +0700

    enhance pricing-page

--- rv-webf/src/views/price/PricePage.vue
+++ rv-webf/src/views/price/PricePage.vue
@@ -26,27 +26,40 @@ import Button from 'primevue/button';
                 <div class="price-container">
                     <div class="price-card">
                         <h3>Basic</h3>
-                        <p class="price">$19/month</p>
-                        <p>Ideal for individuals</p>
-                        <Button>Choose</Button>
+                        <p class="price">$19 / month</p>
+                        <p>250 Full HD images / month </p>
+                        <p>Picture size up to 1920 X 1080 pixels</p>
+                        <p>Batch generation 1</p>
+                        <p>Standard queue speed</p>
+                        <p>Save last 25 images</p>
+                        <Button style="font-weight: 780;">Purchase</Button>
                     </div>
                     <div class="price-card">
-                        <h3>Standard</h3>
-                        <p class="price">$29/month</p>
-                        <p>Best for small teams</p>
-                        <Button>Choose</Button>
+                        <h3>Pro</h3>
+                        <p class="price">$29 / month</p>
+                        <p>600 2k images / month </p>
+                        <p>Picture size up to 2048 X 1080 pixels</p>
+                        <p>Batch generation 3</p>
+                        <p>Faster queue speed</p>
+                        <p>Save up to 100 images</p>
+                        <Button style="font-weight: 780;">Purchase</Button>
                     </div>
                     <div class="price-card">
-                        <h3>Professional</h3>
-                        <p class="price">$49/month</p>
-                        <p>Great for growing companies</p>
-                        <Button>Choose</Button>
+                        <h3>Premium</h3>
+                        <p class="price">$49 / month</p>
+                         <!-- 2000 images per month -->
+                        <p>Unlimited HD images / month </p>
+                        <p>Picture size up to 3840 X 2160 pixels</p>
+                        <p>Batch generation 5</p>
+                        <p>Priority generation queue</p>
+                        <p>Unlimited image history + folders</p>
+                        <Button style="font-weight: 780;">Purchase</Button>
                     </div>
                     <div class="price-card">
                         <h3>Enterprise</h3>
-                        <p class="price">Contact us</p>
-                        <p>Custom solutions</p>
-                        <Button>Contact</Button>
+                        <p class="price">Contact Us</p>
+                        <p style="display: block; text-align: left; line-height: 1.8; padding-bottom: 83px;">For businesses, agencies, or platforms needing high-volume AI-powered room generation, custom tools, or API access.</p>
+                        <Button style="font-weight: 780;">Contact us</Button>
                     </div>
                 </div>
             </section>
