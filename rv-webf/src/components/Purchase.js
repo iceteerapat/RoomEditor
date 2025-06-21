c9f91dfc7d3ec94c24ba9dd8fbc0aa1f8cb48ca9
lankeiceza01 <iceteerapat.k@hotmail.com>
2025-06-21 15:44:40 +0700

    enhance purchase

--- rv-webf/src/components/Purchase.js
+++ rv-webf/src/components/Purchase.js
@@ -2,17 +2,26 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 
+const baseLinkOneAtATime = 'https://buy.stripe.com/test_6oUaEXesE1PCgDn1pFa3u00';
+const baseLinkSubscribeMonthly = 'https://buy.stripe.com/test_5kQ4gzfwIfGs9aV8S7a3u01';
+const baseLinkSubscribeAnnually = 'https://buy.stripe.com/test_bJecN5aco3XK5YJ2tJa3u02';
+
 export function handlePlanSelection(planName, billingCycle) {
-    console.log("billingCycle: ", billingCycle);
+  const email = localStorage.getItem('email');
+  
+  const linkOneAtATime = email ? `${baseLinkOneAtATime}?prefilled_email=${email}` : baseLinkOneAtATime;
+  const linkSubscribeMonthly = email ? `${baseLinkSubscribeMonthly}?prefilled_email=${email}` : baseLinkSubscribeMonthly;
+  const linkSubscribeAnnually = email ? `${baseLinkSubscribeAnnually}?prefilled_email=${email}` : baseLinkSubscribeAnnually;
+
   if (planName === 'Subscription') {
     if (billingCycle === 'Monthly') {
-     window.location.href ='https://buy.stripe.com/test_5kQ4gzfwIfGs9aV8S7a3u01';
+      window.location.href = linkSubscribeMonthly;
     } else {
-      window.location.href = 'https://buy.stripe.com/test_bJecN5aco3XK5YJ2tJa3u02';
+      window.location.href = linkSubscribeAnnually;
     }
   } else if (planName === 'Enterprise') {
     router.push('/contact');
   } else {
-   window.location.href = 'https://buy.stripe.com/test_6oUaEXesE1PCgDn1pFa3u00';
+    window.location.href = linkOneAtATime;
   }
 }
